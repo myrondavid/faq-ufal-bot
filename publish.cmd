cba65fed786a36653a47efe599a58372e032000d
Myron <myrondavid96@gmail.com>
2019-04-03 10:30:50 -0300

    Adicionar arquivos de projeto.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=faq-ufal-bot-Web-Deploy.pubxml -p:Password=iAWvrgSQYpWgz0dTDthxgmiRADsxg8YmYWRjkP4cxKbEt0WnC5D0gMfDsHjn
+
